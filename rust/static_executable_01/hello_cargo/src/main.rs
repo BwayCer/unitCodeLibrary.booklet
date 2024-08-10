de23794eb140c763469c82ea5d6a3b229e9078a8
BwayCer <bwaycer@gmail.com>
2024-08-10 16:20:22 +0800

    feat(rust/static_executable_01): add 建立 Rust 的靜態可執行文件

--- rust/static_executable_01/hello_cargo/src/main.rs
+++ rust/static_executable_01/hello_cargo/src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let name = match env::var("NAME") {
+        Ok(name) => name,
+        Err(_) => match env::args().nth(1) {
+            Some(arg) => arg,
+            None => "world".to_string(),
+        },
+    };
+    println!("Hello, {}!", name);
 }
